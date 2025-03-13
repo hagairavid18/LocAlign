fix distance matrix computation

aligner_dl/models/utils/bbs.py
@@ -74,7 +74,7 @@ def mask_and_normalize_matrix(distance_matrix: torch.Tensor, src_mask: torch.Ten
     distance_matrix = distance_matrix * combined_mask
     distance_matrix = distance_matrix.masked_fill(~combined_mask, float('inf'))
     # t = torch.tensor([guess_best_alpha_torch(src_embedding[i,:][src_mask[i]], dim_num=tar_embedding.shape[-1], transpose=False) for i in range(batch_size)], device=device)
-    t = torch.tensor([torch.tensor(10) for i in range(batch_size)], device=device)
+    t = torch.tensor([torch.tensor(1) for i in range(batch_size)], device=device)
     # print(f't1 {t}')
 
     R = torch.stack([softargmin_rows_torch(distance_matrix[i], t[i]) for i in range(batch_size)], dim=0)