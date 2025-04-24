b529144ad30a348deaf8e006ac6a3afe9114c2d8
hagairavid18 <hagairavid18@gmail.com>
2025-04-24 08:35:47 +0300

    fix kabshc stability

--- aligner_dl/models/utils/kabsch.py
+++ aligner_dl/models/utils/kabsch.py
@@ -54,12 +54,15 @@ def weighted_kabsch_torch(P: torch.Tensor, Q: torch.Tensor, weights: torch.Tenso
              translation vector, and the RMSD.
     """
     assert P.shape == Q.shape, "Matrix dimensions must match"
+    zero_mask = (weights.sum(dim=(1, 2), keepdim=True) == 0)  # Shape: (B, 1, 1)
 
+    # Add small noise to the entire batch if it's all zeros
+    weights = weights + zero_mask * 1e-6
     # rows_normalized_weights = normalize_rows(weights)
     P_weights = torch.sum(weights, axis=1)
     Q_weights = torch.sum(weights, axis=2)
-    weighted_centroids_P = (torch.sum(P * P_weights.unsqueeze(2), axis = 1)  + torch.tensor(1.0))/ (torch.sum(P_weights, axis=1).unsqueeze(dim=1) + torch.tensor(1.0))
-    weighted_centroids_Q = (torch.sum(Q * Q_weights.unsqueeze(2), axis = 1)  + torch.tensor(1.0)) / (torch.sum(Q_weights, axis=1).unsqueeze(dim=1) + torch.tensor(1.0))
+    weighted_centroids_P = (torch.sum(P * P_weights.unsqueeze(2), axis = 1))/ (torch.sum(P_weights, axis=1).unsqueeze(dim=1) )
+    weighted_centroids_Q = (torch.sum(Q * Q_weights.unsqueeze(2), axis = 1)) / (torch.sum(Q_weights, axis=1).unsqueeze(dim=1) )
     
     # Center the points
     p = P - weighted_centroids_P[:, None, :]
